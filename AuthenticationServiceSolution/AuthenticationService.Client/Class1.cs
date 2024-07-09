ef3e2ccbd6249c1c1ed897123497dd9a49460662
Rui Ferreira <ruiferreira91@gmail.com>
2024-07-10 00:07:17 +0100

    commig

---
+++ AuthenticationServiceSolution/AuthenticationService.Client/Class1.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Runtime.CompilerServices;
+
+namespace AuthenticationService.Client
+{
+    //HOW TO GENERATE C# API CLIENT
+    //dotnet tool install --global Microsoft.dotnet-openapi --version 8.0.6
+    //dotnet openapi add url https://petstore.swagger.io/v2/swagger.json --output-file PetStore.json
+    //dotnet build
+    //https://localhost:7048
+   // public class AuthenticationClient : AuthenticationAPIClient
+   // {
+   //     public AuthenticationClient(string baseUrl, HttpClient httpClient) : base(baseUrl, httpClient)
+   //     {
+   //     }
+   //
+   //     public async Task Stuff()
+   //     {
+   //        await base.GetUsersAsync();
+   //     }
+   // }
+}
